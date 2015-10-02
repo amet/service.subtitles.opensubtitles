amet/service.subtitles.opensubtitles

commit 42f6ec95086868cd692c6d670fd7632a7d5db795

Fix for Greek subtitles
Fix for Greek subtitles broken in Kodi 15 and above.

service.subtitles.opensubtitles/service.py
@@ -148,10 +148,10 @@ if params['action'] == 'search' or params['action'] == 'manualsearch':
   for lang in urllib.unquote(params['languages']).decode('utf-8').split(","):
     if lang == "Portuguese (Brazil)":
       lan = "pob"
+    if lang == "Greek":
+      lan = "ell"
     else:
       lan = xbmc.convertLanguage(lang,xbmc.ISO_639_2)
-      if lan == "gre":
-        lan = "ell"
 
     item['3let_language'].append(lan)
 

commit 665873e3bbdee53cfb141af55d046b3407d34b75

Fix for Portuguese (Brazil)
Fix for Portuguese (Brazil) broken by 42f6ec9

service.subtitles.opensubtitles/service.py
@@ -148,7 +148,7 @@ if params['action'] == 'search' or params['action'] == 'manualsearch':
   for lang in urllib.unquote(params['languages']).decode('utf-8').split(","):
     if lang == "Portuguese (Brazil)":
       lan = "pob"
-    if lang == "Greek":
+    elif lang == "Greek":
       lan = "ell"
     else:
       lan = xbmc.convertLanguage(lang,xbmc.ISO_639_2)